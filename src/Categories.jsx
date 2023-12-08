9d6172a085f77b98540b64ba710acfd137f620b7
saidulhasan <saidulhasan562@gmail.com>
2023-12-08 06:46:41 +0600

    initial files

---
+++ src/Categories.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button
+            key={category}
+            type="button"
+            className="btn"
+            onClick={() => {
+              filterItems(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
